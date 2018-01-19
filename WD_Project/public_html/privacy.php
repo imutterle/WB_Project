d3455319d43ea0c7f99730e2723dad1e991aba4e
danielerigon4 <danielerigon4@gmail.com>
2018-01-19 14:46:05 +0100

    aggiunte pagine mancanti

---
+++ WD_Project/public_html/privacy.php
@@ -0,0 +1,19 @@
+<html lang="it">
+<?php include ("head0.html"); ?>
+    <body>
+        
+        <!-- NAVBAR -->
+        
+        <?php include("Navbar0.php"); ?>
+        <div class="container">
+            
+            <div class="path">
+                <a href="homepage.php">Home</a> / <a href="privacy.php">Privacy</a>
+            </div>
+        </div>
+        <!--FOOTER -->
+            
+        <?php include("footer0.php"); ?>
+        
+   </body>
+</html>
