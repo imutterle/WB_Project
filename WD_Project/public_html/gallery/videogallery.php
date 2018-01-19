b44cf44af786444403fe7a926a23a771998b517a
falaswag <ellelaw4@gmail.com>
2018-01-19 17:11:55 +0100

    video gallery

--- WD_Project/public_html/gallery/videogallery.php
+++ WD_Project/public_html/gallery/videogallery.php
@@ -10,9 +10,9 @@
                     <a href="../../homepage.php">Home</a> / <a href="../gallery/gallery.php">Gallery</a> / <a href="../gallery/videogallery.php">Videogallery</a>
             </div>
             <div class="col-md-8 col-md-offset-2">
-                <video class="img-responsive" controls>
-                    <source src="../../video/Flying%20Through%20Space%20(Epic).mp4" type="video/mp4">
-                    <source src="../../video/ftse.webm" type="video/webm">
+                <video class="img-responsive" controls autoplay>
+                    <source src="../../video/Why%20is%20the%20Solar%20System%20Flat_.mp4" type="video/mp4">
+                    <source src="../../video/witssf.webm" type="video/webm">
                     Your browser does not support the video tag.
                 </video>
                 <h3>description</h3>
@@ -20,7 +20,7 @@
             </div>
             <div class="row">
                 <div class="col-md-3">
-                    <video class="img-responsive" controls>
+                    <video class="img-responsive">
                         <source src="../../video/Flying%20Through%20Space%20(Epic).mp4" type="video/mp4">
                         <source src="../../video/ftse.webm" type="video/webm">
                         Your browser does not support the video tag.
@@ -29,23 +29,23 @@
                 
                 <!-- immagine sfocata per indicare che è quella scelta-->
                 <div class="col-md-3" style="filter: blur(2px)"> 
-                    <video class="img-responsive" controls>
-                        <source src="../../video/Flying%20Through%20Space%20(Epic).mp4" type="video/mp4">
-                        <source src="../../video/ftse.webm" type="video/webm">
+                    <video class="img-responsive">
+                        <source src="../../video/Why%20is%20the%20Solar%20System%20Flat_.mp4" type="video/mp4">
+                        <source src="../../video/witssf.webm" type="video/webm">
                         Your browser does not support the video tag.
                     </video>
                 </div>
                 <div class="col-md-3">
-                    <video class="img-responsive" controls>
+                    <video class="img-responsive">
                         <source src="../../video/space%20in%201%20minute%20and%20half..mp4" type="video/mp4">
                         <source src="../../video/si1mah.webm" type="video/webm">
                         Your browser does not support the video tag.
                     </video>
                 </div>
                 <div class="col-md-3">
-                    <video class="img-responsive" controls>
-                        <source src="../../video/Why%20is%20the%20Solar%20System%20Flat_.mp4" type="video/mp4">
-                        <source src="../../video/witssf.webm" type="video/webm">
+                    <video class="img-responsive">
+                        <source src="../../video/Flying%20Through%20Space%20(Epic).mp4" type="video/mp4">
+                        <source src="../../video/ftse.webm" type="video/webm">
                         Your browser does not support the video tag.
                     </video>
                 </div>
