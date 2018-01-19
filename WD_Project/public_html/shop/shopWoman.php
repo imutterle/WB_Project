be3cfafa956e97752db81f6c248e3bb255463388
falaswag <ellelaw4@gmail.com>
2018-01-19 20:14:02 +0100

    sistemato login e shop

--- WD_Project/public_html/shop/shopWoman.php
+++ WD_Project/public_html/shop/shopWoman.php
@@ -10,7 +10,207 @@
             
             <div class="path">
                 <a href="../homepage.php">Home</a> / <a href="shop.php">Shop</a> / <a href="shopWoman.php">Woman products</a>
-            </div>        
+            </div> 
+            
+            <div class="col-md-3">
+                <div class="row">
+                    <div class="list-group">
+                        <a href="shopMan.php" class="list-group-item">Man products</a>
+                        <a href="shopWoman.php" class="list-group-item">Woman products</a>
+                        <a href="shopKid.php" class="list-group-item">Kid products</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <a><button type="button" class="btn" onclick="location.href='cart/cartDesktop.php'">Cart</button></a>
+                </div>
+            </div>
+            
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item1.php"><img class="img-responsive" src="../../img/1.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item1.php">Item One</a>
+                                </h4>
+                                <h5>$15,99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item2.php"><img class="img-responsive" src="../../img/2.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item2.php">Item Two</a>
+                                </h4>
+                                <h5>$24.99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item3.php"><img class="img-responsive" src="../../img/3.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item3.php">Item Three</a>
+                                </h4>
+                                <h5>$99.99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item3.php"><img class="img-responsive" src="../../img/3.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item3.php">Item Four</a>
+                                </h4>
+                                <h5>$99.99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item1.php"><img class="img-responsive" src="../../img/1.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item1.php">Item Five</a>
+                                </h4>
+                                <h5>$15,99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item2.php"><img class="img-responsive" src="../../img/2.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item2.php">Item Six</a>
+                                </h4>
+                                <h5>$24.99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item3.php"><img class="img-responsive" src="../../img/3.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item3.php">Item Seven</a>
+                                </h4>
+                                <h5>$99.99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item3.php"><img class="img-responsive" src="../../img/3.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item3.php">Item Eight</a>
+                                </h4>
+                                <h5>$99.99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item1.php"><img class="img-responsive" src="../../img/1.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item1.php">Item Nine</a>
+                                </h4>
+                                <h5>$15,99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item2.php"><img class="img-responsive" src="../../img/2.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item2.php">Item Ten</a>
+                                </h4>
+                                <h5>$24.99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item3.php"><img class="img-responsive" src="../../img/3.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item3.php">Item Eleven</a>
+                                </h4>
+                                <h5>$99.99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="item">
+                            <div class="row">
+                                <a href="itemMan/item3.php"><img class="img-responsive" src="../../img/3.jpg" alt=""></a>
+                            </div>
+                            <div class="">
+                                <h4 class="">
+                                    <a href="itemMan/item3.php">Item Twelve</a>
+                                </h4>
+                                <h5>$99.99</h5>
+                                <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                        
         </div>
         
         <!--FOOTER -->
