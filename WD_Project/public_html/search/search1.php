13de6af1db4be17254710bbbe4e99e79c7a05587
falaswag <ellelaw4@gmail.com>
2018-01-20 15:26:21 +0100

    ricerca finita

--- WD_Project/public_html/search/search1.php
+++ WD_Project/public_html/search/search1.php
@@ -52,49 +52,69 @@
                 <h3>Search Results:</h3>
             </div>
             
-            <div class="exp-details col-md-8">
-                <h3>Title of section</h3>
-                <h4>Category</h4>
-                <h5>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut sapien est. Vestibulum vitae nibh augue. Nam tristique mauris pretium, ornare nisl non, posuere dolor. Aenean non efficitur ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec interdum dui at massa mollis, a malesuada purus scelerisque.
-                    </p>
-                </h5>                
-                <h4>Tags:#Tag1,#Tag2,#Tag3</h4>
+            <div class="exp-details col-md-12">
+                <div class="col-md-8">
+                    <h3>Title of section</h3>
+                    <h4>Category</h4>
+                    <h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut sapien est. Vestibulum vitae nibh augue. Nam tristique mauris pretium, ornare nisl non, posuere dolor. Aenean non efficitur ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec interdum dui at massa mollis, a malesuada purus scelerisque.
+                        </p>
+                    </h5>                
+                    <h4>Tags:#Tag1,#Tag2,#Tag3</h4>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-responsive" src="../../img/1.jpg">
+                </div>
             </div>
             
-            <div class="exp-details col-md-8">
-                <h3>Title of section</h3>
-                <h4>Category</h4>
-                <h5>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut sapien est. Vestibulum vitae nibh augue. Nam tristique mauris pretium, ornare nisl non, posuere dolor. Aenean non efficitur ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec interdum dui at massa mollis, a malesuada purus scelerisque.
-                    </p>
-                </h5>                
-                <h4>Tags:#Tag1,#Tag2,#Tag3</h4>
+            <div class="exp-details col-md-12">
+                <div class="col-md-8">
+                    <h3>Title of section</h3>
+                    <h4>Category</h4>
+                    <h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut sapien est. Vestibulum vitae nibh augue. Nam tristique mauris pretium, ornare nisl non, posuere dolor. Aenean non efficitur ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec interdum dui at massa mollis, a malesuada purus scelerisque.
+                        </p>
+                    </h5>                
+                    <h4>Tags:#Tag1,#Tag2,#Tag3</h4>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-responsive" src="../../img/2.jpg">
+                </div>
             </div>
             
-            <div class="exp-details col-md-8">
-                <h3>Title of section</h3>
-                <h4>Category</h4>
-                <h5>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut sapien est. Vestibulum vitae nibh augue. Nam tristique mauris pretium, ornare nisl non, posuere dolor. Aenean non efficitur ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec interdum dui at massa mollis, a malesuada purus scelerisque.
-                    </p>
-                </h5>                
-                <h4>Tags:#Tag1,#Tag2,#Tag3</h4>
+            <div class="exp-details col-md-12">
+                <div class="col-md-8">
+                    <h3>Title of section</h3>
+                    <h4>Category</h4>
+                    <h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut sapien est. Vestibulum vitae nibh augue. Nam tristique mauris pretium, ornare nisl non, posuere dolor. Aenean non efficitur ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec interdum dui at massa mollis, a malesuada purus scelerisque.
+                        </p>
+                    </h5>                
+                    <h4>Tags:#Tag1,#Tag2,#Tag3</h4>
+                </div>
+                <div class="col-md-4">
+                </div>
             </div>
             
-            <div class="exp-details col-md-8">
-                <h3>Title of section</h3>
-                <h4>Category</h4>
-                <h5>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut sapien est. Vestibulum vitae nibh augue. Nam tristique mauris pretium, ornare nisl non, posuere dolor. Aenean non efficitur ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec interdum dui at massa mollis, a malesuada purus scelerisque.
-                    </p>
-                </h5>                
-                <h4>Tags:#Tag1,#Tag2,#Tag3</h4>
+            <div class="exp-details col-md-12">
+                <div class="col-md-8">
+                    <h3>Title of section</h3>
+                    <h4>Category</h4>
+                    <h5>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut sapien est. Vestibulum vitae nibh augue. Nam tristique mauris pretium, ornare nisl non, posuere dolor. Aenean non efficitur ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec interdum dui at massa mollis, a malesuada purus scelerisque.
+                        </p>
+                    </h5>                
+                    <h4>Tags:#Tag1,#Tag2,#Tag3</h4>
+                </div>
+                <div class="col-md-4">
+                    <img class="img-responsive" src="../../img/3.jpg">
+                </div>
             </div>
+            
         </div>
         <div class="divEdett"></div>
         <!-- FOOTER -->
