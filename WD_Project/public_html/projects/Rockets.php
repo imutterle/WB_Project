acea9b6f9136e38009a3dbb8f0e2113c58fe65d4
imutterle <mutterleilaria@gmail.com>
2018-01-20 16:56:20 +0100

    update

--- WD_Project/public_html/projects/Rockets.php
+++ WD_Project/public_html/projects/Rockets.php
@@ -21,25 +21,21 @@
                             <h3>Rocket Denny</h3>
                     </div>
                     <div class="col-md-4">
-
                             <img class="img-responsive" src="../../img/rockets2.jpg">
                             <h3>Rocket Daniele</h3>
-
                     </div>
                     <div class="col-md-4">
                             <img class="img-responsive" src="../../img/rockets3.jpg">
                             <h3>Rocket Luca</h3>
                     </div>
-
               </div>
+
               <div class="row">
                       <div class="col-md-offset-4 col-md-4">
                               <img class="img-responsive" src="../../img/rockets4.jpg">
                               <h3>Rocket Ilaria</h3>
                       </div>
-
-
-                </div>
+              </div>
         </div>
 
         <!--FOOTER -->
