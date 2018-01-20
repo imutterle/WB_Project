1506d58c4c511312d788f573cd149325928e121a
imutterle <mutterleilaria@gmail.com>
2018-01-20 15:38:04 +0100

    benedetti progetti

--- WD_Project/public_html/projects/SpaceSuit.php
+++ WD_Project/public_html/projects/SpaceSuit.php
@@ -1,23 +1,48 @@
 <html lang="it">
     <?php include ("../head1.html"); ?>
     <body>
-        
+
         <!-- NAVBAR -->
         <?php include("../Navbar1.php"); ?>
-        
-        <div class="container"> 
+
+        <div class="container">
             <div class="path">
                 <a href="../homepage.php">Home</a> / <a href="../Projects.php">Projects</a> / <a href="SpaceSuit.php">Space Suit</a>
             </div>
-            
+
             <div class="spaceSuit">
                 <h1>Title</h1>
                 <h3>Description of the section</h3>
                 <h5>Lorem ...</h5>
             </div>
+
+            <div class="row">
+                    <div class="col-md-3">
+                            <img class="img-responsive" src="../../img/spacesuit1.jpg">
+                            <h3>History</h3>
+                    </div>
+                    <div class="col-md-3">
+
+                            <img class="img-responsive" src="../../img/spacesuit2.jpg">
+                            <h3>Materials</h3>
+
+                    </div>
+                    <div class="col-md-3">
+
+                            <img class="img-responsive" src="../../img/spacesuit3.jpg">
+                            <h3> Science studies for spacesuits</h3>
+
+                    </div>
+                    <div class="col-md-3">
+
+                            <img class="img-responsive" src="../../img/spaceProjects4.jpg">
+                            <h3>Suborbital Spaceflight</h3>
+
+                    </div>
+              </div>
         </div>
-        
-        <!--FOOTER -->    
+
+        <!--FOOTER -->
         <?php include("../footer1.php"); ?>
     </body>
 </html>
