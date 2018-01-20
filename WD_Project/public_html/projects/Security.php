7e3dfbdb57ca15a7e893ca583372400d18602062
imutterle <mutterleilaria@gmail.com>
2018-01-20 16:32:32 +0100

    security

--- WD_Project/public_html/projects/Security.php
+++ WD_Project/public_html/projects/Security.php
@@ -1,23 +1,51 @@
 <html lang="it">
     <?php include ("../head1.html"); ?>
     <body>
-        
+
         <!-- NAVBAR -->
         <?php include("../Navbar1.php"); ?>
-        
-        <div class="container"> 
+
+        <div class="container">
             <div class="path">
                 <a href="../homepage.php">Home</a> / <a href="../Projects.php">Projects</a> / <a href="Security.php">Security</a>
             </div>
-            
+
             <div class="security">
                 <h1>Title</h1>
                 <h3>Description of the section</h3>
                 <h5>Lorem ...</h5>
             </div>
+
+            <div class="row">
+                    <div class="col-md-4">
+                            <img class="img-responsive" src="../../img/security1.jpg">
+                            <h3>Security classes - Beginers</h3>
+                    </div>
+                    <div class="col-md-4">
+
+                            <img class="img-responsive" src="../../img/security2.jpg">
+                            <h3>Security classes - Intermediates</h3>
+
+                    </div>
+                    <div class="col-md-4">
+
+                            <img class="img-responsive" src="../../img/security3.jpg">
+                            <h3>Security classes - Experts</h3>
+
+                    </div>
+
+              </div>
+
+              <div class="row">
+                      <div class="col-md-offset-4 col-md-4">
+                              <img class="img-responsive" src="../../img/security4.jpg">
+                              <h3>Security - our rules</h3>
+                      </div>
+              </div>
+
         </div>
-        
-        <!--FOOTER -->    
+
+        <!--FOOTER -->
         <?php include("../footer1.php"); ?>
     </body>
 </html>
