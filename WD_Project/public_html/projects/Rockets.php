dd4f55a7fd1fe1ed90016cc32bb3029938686ac7
imutterle <mutterleilaria@gmail.com>
2018-01-20 16:53:36 +0100

    rockets

--- WD_Project/public_html/projects/Rockets.php
+++ WD_Project/public_html/projects/Rockets.php
@@ -1,23 +1,48 @@
 <html lang="it">
     <?php include ("../head1.html"); ?>
     <body>
-        
+
         <!-- NAVBAR -->
         <?php include("../Navbar1.php"); ?>
-        
-        <div class="container"> 
+
+        <div class="container">
             <div class="path">
                 <a href="../homepage.php">Home</a> / <a href="../Projects.php">Projects</a> / <a href="Rockets.php">Rockets</a>
             </div>
-            
+
             <div class="rockets">
                 <h1>Title</h1>
                 <h3>Description of the section</h3>
                 <h5>Lorem ...</h5>
             </div>
+            <div class="row">
+                    <div class="col-md-4">
+                            <img class="img-responsive" src="../../img/rockets1.jpg">
+                            <h3>Rocket Denny</h3>
+                    </div>
+                    <div class="col-md-4">
+
+                            <img class="img-responsive" src="../../img/rockets2.jpg">
+                            <h3>Rocket Daniele</h3>
+
+                    </div>
+                    <div class="col-md-4">
+                            <img class="img-responsive" src="../../img/rockets3.jpg">
+                            <h3>Rocket Luca</h3>
+                    </div>
+
+              </div>
+              <div class="row">
+                      <div class="col-md-offset-4 col-md-4">
+                              <img class="img-responsive" src="../../img/rockets4.jpg">
+                              <h3>Rocket Ilaria</h3>
+                      </div>
+
+
+                </div>
         </div>
-        
-        <!--FOOTER -->    
+
+        <!--FOOTER -->
         <?php include("../footer1.php"); ?>
     </body>
 </html>
