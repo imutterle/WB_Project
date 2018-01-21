505322298dd01c2e056780fb43b4b851d5b945e4
imutterle <mutterleilaria@gmail.com>
2018-01-21 12:20:40 +0100

    aggiunto link per navigazione trasversale

--- WD_Project/public_html/dettaglioExperiences/dettaglioCircumlunarMission.php
+++ WD_Project/public_html/dettaglioExperiences/dettaglioCircumlunarMission.php
@@ -2,31 +2,32 @@
     <?php include ("../head1.html"); ?>
     <body>
         <!-- NAVBAR -->
-        
+
         <?php include("../Navbar1.php"); ?>
-        
+
         <div class="container">
-            
+
             <div class="path">
-                <a href="../homepage.php">Home</a> / <a href="../Projects.php">Projects</a> / <a href="../projects/SpatialTourism.php">Spatial Tourism</a> / <a href="dettaglioCircumlunarMission.php">Circumlunar Mission</a> 
+                <a href="../homepage.php">Home</a> / <a href="../Projects.php">Projects</a> / <a href="../projects/SpatialTourism.php">Spatial Tourism</a> / <a href="dettaglioCircumlunarMission.php">Circumlunar Mission</a>
             </div>
-            
+
             <div>
                 <h1>Circumlunar Mission</h1>
             </div>
             <div class="col-md-5">
-                <img class="img-responsive" src="../../img/spaceProjects3.jpg">    
+                <img class="img-responsive" src="../../img/spaceProjects3.jpg">
             </div>
             <div class="col-md-7">
                 <h3>Description</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est mi, lacinia viverra nunc sit amet, feugiat interdum tortor. Duis fermentum magna metus, eget aliquet quam hendrerit et. Aliquam mattis sapien ac libero vestibulum, vitae porta magna tristique. Donec eget felis augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed scelerisque feugiat dolor, vitae lobortis purus tempus nec. Praesent posuere leo vel commodo suscipit. Integer tincidunt sapien nunc, eu sagittis justo congue at. Maecenas tortor nisi, malesuada id pharetra tincidunt, scelerisque non nulla. </p>
+                <p> See also our new <a href="../projects/Rockets.php">Rockets</a> which are involved with this mission!</p>
             </div>
             <div class="col-md-12">
                 <h2>Mission Details</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est mi, lacinia viverra nunc sit amet, feugiat interdum tortor. Duis fermentum magna metus, eget aliquet quam hendrerit et. Aliquam mattis sapien ac libero vestibulum, vitae porta magna tristique. Donec eget felis augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed scelerisque feugiat dolor, vitae lobortis purus tempus nec. Praesent posuere leo vel commodo suscipit. Integer tincidunt sapien nunc, eu sagittis justo congue at. Maecenas tortor nisi, malesuada id pharetra tincidunt, scelerisque non nulla. </p>
             </div>
             <div class="exp-details col-md-12">
-                <h4>Prices: &#36 total</h4>
+                <h4>Prices: &#36 total </h4>
             </div>
             <div class="exp-details col-md-12">
                 <h4>Contact Directly: <a href="#">example@example.com</a></h4>
@@ -49,8 +50,8 @@
         <div class="divEdett"></div>
 
         <!--FOOTER -->
-        
+
         <?php include("../footer1.php"); ?>
-        
+
     </body>
 </html>
